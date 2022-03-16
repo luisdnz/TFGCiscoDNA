679077a007d4209a4970a1f73a5b9b9c8fa6f884
Luis Dominguez <90524773+luisdnz@users.noreply.github.com>
2022-03-16 10:40:32 -0400

    Actualizados archivos creacion y configuracion lab

---
+++ genie_prueba.py
@@ -0,0 +1,18 @@
+from genie.conf import Genie
+from genie.conf.base import Interface
+
+testbed = Genie.init("testbed.yaml")
+
+dist_rtr01 = testbed.devices["dist-rtr02"]
+dist_rtr01.connect()
+
+new_loopback = Interface(device = dist_rtr01, name="Loopback12")
+new_loopback.ipv4 = "10.255.255.2"
+new_loopback.ipv4.netmask = "255.255.255.255"
+new_loopback.description = "Created with pyATS and Objects"
+new_loopback.shutdown = False
+
+
+
+new_loopback.build_config(apply=True)
+dist_rtr01.disconnect()
--- retrieve_node_conf.py
+++ retrieve_node_conf.py
@@ -1,13 +1,13 @@
 from virl2_client import ClientLibrary
+import genie
 
 client = ClientLibrary(url="10.10.20.161", username="developer", password="C1sco12345", ssl_verify=False)
 client.is_system_ready(wait=True)
-
 #Con esto podemos crear un lab con el nombre que queramos.
 #Si nos queremos unir a un lab ya creado usaríamos: client.join_existing_lab(labId)
 lab = client.find_labs_by_title("Small NXOS/IOSXE Network")[0]
 
 pyats_testbeb = lab.get_pyats_testbed()
 
-with open("lab_tested.yaml", "w") as f:
+with open("testbed.yaml", "w") as f:
     f.write(pyats_testbeb)
