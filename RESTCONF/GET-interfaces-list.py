0dba8d9bae12acecc43db92d1df291d00f3cc409
luisdnz <luis.dominguezr@alumnos.upm.es>
2022-05-02 21:52:27 +0200

    Actualizados los yaml
    Los routers de CML a veces fallan e, incluso con la configuración correcta, no funciona RESTCONF. Estos yaml de los labs incluyen routers que funcionan bien.

--- RESTCONF/GET-interfaces-list.py
+++ RESTCONF/GET-interfaces-list.py
@@ -3,7 +3,7 @@ import json
 from pprint import pprint
 
 distr_rt_1 = {
-    "ip" : "10.10.20.176",
+    "ip" : "10.10.20.185",
     "port": "443",
     "username":"cisco",
     "password":"cisco"    
