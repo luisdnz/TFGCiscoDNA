91a6393b67da6bd5c3ba1c476855d76e1db5a07e
luisdnz <luis.dominguezr@alumnos.upm.es>
2022-05-02 19:59:50 +0200

    Añadida la tabla de direccionamientos

--- RESTCONF/PUT-ospf-config.py
+++ RESTCONF/PUT-ospf-config.py
@@ -3,7 +3,7 @@ import json
 from pprint import pprint
 
 distr_rt_1 = {
-    "ip" : "10.10.20.183",
+    "ip" : "10.10.20.184",
     "port": "443",
     "username":"cisco",
     "password":"cisco"    
